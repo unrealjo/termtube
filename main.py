a18b8e3b4e0023a809ae24198f6d2c1f1314acfd
Youssef Eddaif <yeddaif18@gmail.com>
2022-02-27 18:24:42 +0100

    write only titles to result file and get index of selected video

--- main.py
+++ main.py
@@ -5,30 +5,33 @@ import subprocess
 opts, args = getopt.getopt(sys.argv[1:],"hdfr")
 query= '+'.join(args) # join args with + as separator
 pips = ('fzf', 'dmenu', 'rofi -dmenu') # List of programs to pip results to
-if len(opts) > 0 : # means if the option is specitied
+if len(opts) > 0 : # means if the option is specified
     opt = opts[0][0]
 else:
     opt = ''
 def yt(search_query, pip2):
-    vidIds, num = Search(search_query).get_info()   
+    vidIds, titles = Search(search_query).get_info()   
 
     # Writing results to temporary file
     file = open('/tmp/results', 'w') 
-    for i in range(0 ,num*2 ,2):
-        file.write(f"{vidIds[i]} [{vidIds[i+1]}]\n")
+    i = 0
+    for title in titles:
+        file.write(f"{i} [{title}]\n") # Writing index and title of each video
+        i += 1
     file.close()
 
     # Getting chose from user
     runer = subprocess.Popen(f"cat /tmp/results | {pip2}", shell=True, stdout=subprocess.PIPE)
     result = str(runer.stdout.read())
-    return "https://youtube.com/watch?v=" + result[2:13] 
+    index = int(result[2:3]) # Get index of selected video
+    return "https://youtube.com/watch?v=" + vidIds[index] 
 
 video = ''
 if opt == '-h':
     print("""termtube [options] [search_query]
     -h : Help
-    -f to get result in fzf
     -d to get result in dmenu
+    -f to get result in fzf
     -r to get result in rofi
     """)
 elif opt == '-f':
