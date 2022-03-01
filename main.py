ca1ddf087b8bb7f2f961734e44c70b210cdae37d
Youssef Eddaif <yeddaif18@gmail.com>
2022-03-02 00:26:20 +0100

    delete unsed imports

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
-from tools import Search # My implementation instaed of pytube's one
-import getopt, sys
-import subprocess
+from subprocess import run, PIPE, Popen
+from requests import get
+from json import loads
+import argparse
 
-opts, args = getopt.getopt(sys.argv[1:],"hdfr")
-query= '+'.join(args) # join args with + as separator
-pips = ('fzf', 'dmenu', 'rofi -dmenu') # List of programs to pip results to
-if len(opts) > 0 : # means if the option is specified
-    opt = opts[0][0]
-else:
-    opt = ''
-def yt(search_query, pip2):
-    vidIds, titles = Search(search_query).get_info()   
+"""
+I implemnted my own search class
+bescause I found search class from pytube somehow slow
+and a bit complecated :) and also for the seek of practicing
+in python
+It's not a fork of yt-fzf !!
+"""
+
+def search(query, pipe_to):
+    video_id = []
+    titles = []
+    resut_query = get(F'https://vid.puffyan.us/api/v1/search?q={query}')
+    data = loads(resut_query.content)
+    for i in range(len(data)):
+            video_id.append(data[i]['videoId'])
+            titles.append(data[i]['title'])
 
     # Writing results to temporary file
-    file = open('/tmp/results', 'w') 
+    file = open("/tmp/results","w", encoding="utf-8")
     i = 0
     for title in titles:
         file.write(f"{i} [{title}]\n") # Writing index and title of each video
@@ -21,27 +29,28 @@ def yt(search_query, pip2):
     file.close()
 
     # Getting chose from user
-    runer = subprocess.Popen(f"cat /tmp/results | {pip2}", shell=True, stdout=subprocess.PIPE)
-    result = str(runer.stdout.read())
+    get_chosed_videoId = Popen(f"cat /tmp/results | {pipe_to}", shell=True, stdout=PIPE)
+    result = str(get_chosed_videoId.stdout.read())
     index = int(result[2:3]) # Get index of selected video
-    return "https://youtube.com/watch?v=" + vidIds[index] 
+    return "https://youtube.com/watch?v=" + video_id[index] 
+
+# Thaks to awesome people over reddit (r/python)
+
+parser = argparse.ArgumentParser(description="Search and play Youtube videos from CLI")
+parser.add_argument("query", type=str , help="Query to search for", nargs='+')
+parser.add_argument("-f", default=False, action="store_true", help="Run throught fzf")
+parser.add_argument("-d", default=False, action="store_true", help="Run throught dmenu")
+parser.add_argument("-r", default=False, action="store_true", help="Run throught rofi")
+args = parser.parse_args()
 
 video = ''
-if opt == '-h':
-    print("""termtube [options] [search_query]
-    -h : Help
-    -d to get result in dmenu
-    -f to get result in fzf
-    -r to get result in rofi
-    """)
-elif opt == '-f':
-    video = yt(query,pips[0])
-    subprocess.run(['/bin/mpv', video])
-elif opt == '-d':
-    video = yt(query,pips[1])
-    subprocess.run(['/bin/mpv', video])
-elif opt == '-r':
-    video = yt(query,pips[2])
-    subprocess.run(['/bin/mpv', video])
-elif opt == '':
-    print("You need to specity an option")
+query = '+'.join(args.query)
+if args.d:
+    video = search(query,'dmenu')
+if args.f:
+    video = search(query,'fzf')
+if args.r:
+    video = search(query,'rofi -dmenu')
+if video != '':
+    run(['/bin/mpv', video])
+
